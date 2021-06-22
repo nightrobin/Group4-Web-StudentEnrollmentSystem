34cfdaf86d16debbe611d5fd2d1dcb7d8894aabc
dhargrefiel <digrefiel2020@plm.edu.ph>
2021-06-22 23:27:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -94,7 +94,7 @@
 					<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center" id="bg">
 					  <div class="col-md-5 p-lg-5 mx-auto my-5">
 						<h1 class="display-4 font-weight-normal" style="color:white;">Enroll Now!</h1>
-						<p class="lead font-weight-normal" style="color:white;">Robinson University is now accepting enrollees for SY 2020-2021, starting from August 25 - September 1 2021. </p>
+						<p class="lead font-weight-normal" style="color:white;">Robinson University is now accepting enrollees for SY 2020-2021, starting from January 1 - February 28, 2021. </p>
 						<a class="btn btn-outline-secondary btn-outline-secondary:hover" href="login.php" id="comingSoonBtn" style="color:white;">Proceed</a>
 					  </div>
 					  <div class="product-device box-shadow d-none d-md-block"></div>
